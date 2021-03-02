c0344ac37d9758c50fe9267a65c9c858bdd9e7db
imad <imadatyatalah@gmail.com>
2021-03-02 19:53:17 +0100

    Added src directory

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import type { AppProps } from 'next/app'
+
+import "../styles/globals.css";
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Component {...pageProps} />
+  </>
+);
+
+export default MyApp;
