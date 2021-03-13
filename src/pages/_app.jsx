57d87d7b0b6075220d71b33ebeab9dc398abf533
imad <imadatyatalah@gmail.com>
2021-03-13 19:14:15 +0100

    Wrapping the Component with QueryClientProvider and Hydrate

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,8 @@
+import { useRef } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { Hydrate } from "react-query/hydration";
 
 import { theme } from "../theme";
 
@@ -7,12 +11,26 @@ import "@fontsource/poppins/latin-500.css";
 import "@fontsource/poppins/latin-600.css";
 import "@fontsource/poppins/latin-700.css";
 
-const MyApp = ({ Component, pageProps }) => (
-  <>
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  </>
-);
+const queryClient = new QueryClient();
+
+const MyApp = ({ Component, pageProps }) => {
+  const queryClientRef = useRef();
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient();
+  }
+
+  return (
+    <>
+      <ChakraProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={pageProps.dehydratedState}>
+            <Component {...pageProps} />
+          </Hydrate>
+          <ReactQueryDevtools />
+        </QueryClientProvider>
+      </ChakraProvider>
+    </>
+  );
+};
 
 export default MyApp;
