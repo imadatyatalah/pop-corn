4cd3f85c7db47c73c1f31068a5379d69fbee4051
imad <imadatyatalah@gmail.com>
2021-03-13 19:12:38 +0100

    Added type prop

--- src/lib/movies.jsx
+++ src/lib/movies.jsx
@@ -1,7 +1,7 @@
 import { fetcher, API_KEY, BASE_URL } from "../../config";
 
-export const getPopularMovies = async (page) => {
+export const getMovies = async (type, page) => {
   return await fetcher(
-    `${BASE_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${page}`
+    `${BASE_URL}movie/${type}?api_key=${API_KEY}&language=en-US&page=${page}`
   );
 };
