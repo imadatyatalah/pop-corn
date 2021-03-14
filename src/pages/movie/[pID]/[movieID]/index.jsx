42d644f3c1e6e694abb7a9b4444969daf7584f01
imad <imadatyatalah@gmail.com>
2021-03-14 19:08:19 +0100

    Added movie details page

---
+++ src/pages/movie/[pID]/[movieID]/index.jsx
@@ -0,0 +1,35 @@
+import { QueryClient, useQuery } from "react-query";
+import { dehydrate } from "react-query/hydration";
+import { useRouter } from "next/router";
+
+import { getMovieDetails } from "../../../../lib/movies";
+import HeroSection from "../../../../components/detailsPage/heroSection";
+
+const Movie = () => {
+  const router = useRouter();
+  const { query } = router;
+
+  const { data } = useQuery(["movieID", query.movieID], () =>
+    getMovieDetails(query.movieID)
+  );
+
+  return (
+    <>
+      <HeroSection data={data} />
+    </>
+  );
+};
+
+export const getServerSideProps = async ({ params }) => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(["movieID", params.movieID], () =>
+    getMovieDetails(params.movieID)
+  );
+
+  return {
+    props: { dehydratedState: dehydrate(queryClient) },
+  };
+};
+
+export default Movie;
