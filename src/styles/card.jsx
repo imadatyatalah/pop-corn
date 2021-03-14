eb3a187bceea9a8af653c88a67765718458c2c36
imad <imadatyatalah@gmail.com>
2021-03-14 19:05:17 +0100

    card.jsx => index.jsx

--- src/styles/card.jsx
+++
@@ -1,6 +0,0 @@
-import NextImage from "next/image";
-import styled from "@emotion/styled";
-
-export const StyledNextImage = styled(NextImage)`
-  border-radius: 0.375rem;
-`;
---
+++ src/styles/index.jsx
@@ -0,0 +1,27 @@
+import { css } from "@emotion/react";
+import NextImage from "next/image";
+import styled from "@emotion/styled";
+
+export const StyledNextImage = styled(NextImage)`
+  border-radius: 0.375rem;
+`;
+
+export const TagLine = styled.i`
+  font-weight: 300;
+`;
+
+export const Title = styled.strong`
+  font-weight: 600;
+`;
+
+export const InfoCardStyles = css`
+  background: rgba(255, 255, 255, 0.75);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(8px);
+  -webkit-backdrop-filter: blur(8px);
+
+  @supports not (backdrop-filter: none) {
+    backdrop-filter: none;
+    background: rgba(255, 255, 255, 1); /* Opacity 100% */
+  }
+`;
