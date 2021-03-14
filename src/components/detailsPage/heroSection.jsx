f5888a0b7f3d625f14e78d9d912bbaffc6f1835e
imad <imadatyatalah@gmail.com>
2021-03-14 19:09:45 +0100

    Added hero section of DetailsPage

---
+++ src/components/detailsPage/heroSection.jsx
@@ -0,0 +1,34 @@
+import { Box } from "@chakra-ui/react";
+
+import { IMAGE_BASE_URL, LG_POSTER_SIZE } from "../../../config";
+import { StyledNextImage } from "../../styles";
+import Backdrop from "./backdrop";
+import InfoCard from "./infoCard";
+
+const DetailsPage = ({ data }) => {
+  return (
+    <>
+      <Backdrop backdropPath={data.backdrop_path}>
+        <Box
+          d="flex"
+          justifyContent={{ base: "center", lg: "flex-start" }}
+          flexDir={{ base: "column", lg: "row" }}
+          py={{ base: "3rem", lg: "2rem" }}
+        >
+          <Box d="flex" alignSelf="center" shadow="2xl">
+            <StyledNextImage
+              src={`${IMAGE_BASE_URL}${LG_POSTER_SIZE}${data.poster_path}`}
+              width="342"
+              height="513"
+              alt={data.title}
+              layout="fixed"
+            />
+          </Box>
+          <InfoCard data={data} />
+        </Box>
+      </Backdrop>
+    </>
+  );
+};
+
+export default DetailsPage;
