04bec9955084b12bb85e061bc480671b1dc3d09c
imad <imadatyatalah@gmail.com>
2021-03-14 13:53:26 +0100

    Using MediaCards

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,29 +1,15 @@
 import { QueryClient, useQuery } from "react-query";
 import { dehydrate } from "react-query/hydration";
-import { Box } from "@chakra-ui/react";
 
 import { getMovies } from "../lib/movies";
-import { IMAGE_BASE_URL, LG_POSTER_SIZE } from "../../config";
-import { StyledNextImage } from "../styles/card";
-import CardsContainer from "../components/UI/cardsContainer";
+import { MediaCards as MoviesCards } from "../components/UI/mediaCards";
 
 const Home = () => {
   const { data } = useQuery("movies", () => getMovies("popular", 1));
 
   return (
     <>
-      <CardsContainer>
-        {data?.results.map((movie) => (
-          <Box d="flex" rounded="md" key={movie.title}>
-            <StyledNextImage
-              src={`${IMAGE_BASE_URL}${LG_POSTER_SIZE}${movie.poster_path}`}
-              width="256"
-              height="384"
-              alt={movie.title}
-            />
-          </Box>
-        ))}
-      </CardsContainer>
+      <MoviesCards data={data} mediaType="movie" />
     </>
   );
 };
