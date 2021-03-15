65b3ee4d79ddf51c5b63a41ff305e9492e70ffe7
imad <imadatyatalah@gmail.com>
2021-03-15 17:31:57 +0100

    Pass data to 4 staticly generated pages

--- src/pages/movie/[pID]/index.jsx
+++ src/pages/movie/[pID]/index.jsx
@@ -1,5 +1,23 @@
+import { QueryClient, useQuery } from "react-query";
+import { dehydrate } from "react-query/hydration";
+import { useRouter } from "next/router";
+
+import { getMovies } from "../../../lib/movies";
+import { MediaCards as MoviesCards } from "../../../components/UI/mediaCards";
+
 const PID = () => {
-  return <></>;
+  const router = useRouter();
+  const { query } = router;
+
+  const { data } = useQuery(["movies", query.pID], () =>
+    getMovies(query.pID, 1)
+  );
+
+  return (
+    <>
+      <MoviesCards data={data} mediaType="movie" />
+    </>
+  );
 };
 
 export const getStaticPaths = async () => {
@@ -14,11 +32,16 @@ export const getStaticPaths = async () => {
   };
 };
 
-export const getStaticProps = async () => {
+export const getStaticProps = async ({ params }) => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(["movies", params.pID], () =>
+    getMovies(params.pID, 1)
+  );
+
   return {
-    props: {
-      data: null,
-    },
+    props: { dehydratedState: dehydrate(queryClient) },
+    revalidate: 1,
   };
 };
 
