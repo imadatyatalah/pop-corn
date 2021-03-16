917c856564017c2d25dc3a0123d21ab111966cfe
imad <imadatyatalah@gmail.com>
2021-03-16 17:58:53 +0100

    Added poster fallback image

---
+++ src/components/UI/fallBack.jsx
@@ -0,0 +1,14 @@
+import { StyledPosterFallBack } from "./styles/fallBack.styles";
+
+export const PosterFallBack = () => (
+  <>
+    <StyledPosterFallBack
+      src="/image.svg"
+      width="342"
+      height="513"
+      alt="image-icon ionicons ionicons-icon"
+      layout="fixed"
+      quality="50"
+    />
+  </>
+);
