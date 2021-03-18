e7af9818daa6dfebe871905030f21dff2d7523aa
imad <imadatyatalah@gmail.com>
2021-03-18 19:34:46 +0100

    Remove layout prop

--- src/components/UI/fallBack.jsx
+++ src/components/UI/fallBack.jsx
@@ -1,14 +1,14 @@
 import { StyledPosterFallBack } from "./styles/fallBack.styles";
 
-export const PosterFallBack = () => (
+export const PosterFallBack = (props) => (
   <>
     <StyledPosterFallBack
       src="/image.svg"
       width="342"
       height="513"
       alt="image-icon ionicons ionicons-icon"
-      layout="fixed"
       quality="50"
+      {...props}
     />
   </>
 );
