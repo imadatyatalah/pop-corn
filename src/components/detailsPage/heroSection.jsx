32e7b7271666897167c9de53298004d04269adec
imad <imadatyatalah@gmail.com>
2021-03-19 15:38:16 +0100

    Added PersonDetailsPage component

--- src/components/detailsPage/heroSection.jsx
+++ src/components/detailsPage/heroSection.jsx
@@ -2,13 +2,13 @@ import { Box, Button } from "@chakra-ui/react";
 import PropTypes from "prop-types";
 import NextLink from "next/link";
 
-import { IMAGE_BASE_URL, LG_POSTER_SIZE } from "../../../config";
+import { IMAGE_BASE_URL, LG_POSTER_SIZE, MD_STILL_SIZE } from "../../../config";
 import { StyledNextImage } from "../../styles";
-import { PosterFallBack } from "../UI/fallBack";
+import { PosterFallBack, ProfileFallBack } from "../UI/fallBack";
 import Backdrop from "./backdrop";
-import InfoCard from "./infoCard";
+import { MediaInfoCard, PersonInfoCard } from "./infoCard";
 
-const DetailsPage = ({ data, backBtnPath }) => {
+export const MediaDetailsPage = ({ data, backBtnPath }) => {
   const { backdrop_path, poster_path, title, name } = data;
 
   return (
@@ -28,7 +28,7 @@ const DetailsPage = ({ data, backBtnPath }) => {
 
         <Box
           d="flex"
-          justifyContent={{ base: "center", lg: "flex-start" }}
+          justifyContent="center"
           flexDir={{ base: "column", lg: "row" }}
           py={{ base: "3rem", lg: "2rem" }}
         >
@@ -54,20 +54,80 @@ const DetailsPage = ({ data, backBtnPath }) => {
             )}
           </Box>
 
-          <InfoCard data={data} />
+          <MediaInfoCard data={data} />
         </Box>
       </Backdrop>
     </>
   );
 };
 
-DetailsPage.defaultProps = {
+export const PersonDetailsPage = ({ data, backBtnPath }) => {
+  const { backdrop_path, profile_path, name } = data;
+
+  return (
+    <>
+      <Backdrop backdropPath={backdrop_path}>
+        <NextLink href={backBtnPath}>
+          <a>
+            <Button
+              px="8"
+              colorScheme="facebook"
+              mt={{ base: "3rem", lg: "2rem" }}
+            >
+              Back
+            </Button>
+          </a>
+        </NextLink>
+
+        <Box
+          d="flex"
+          justifyContent="center"
+          flexDir={{ base: "column", lg: "row" }}
+          py={{ base: "3rem", lg: "2rem" }}
+        >
+          <Box
+            d="flex"
+            rounded="md"
+            alignSelf="center"
+            shadow="2xl"
+            bgColor="gray.500"
+            aria-label={`${name} Profile image`}
+          >
+            {profile_path ? (
+              <StyledNextImage
+                src={`${IMAGE_BASE_URL}${MD_STILL_SIZE}${profile_path}`}
+                width="342"
+                height="513"
+                alt={name}
+                layout="fixed"
+                quality="60"
+              />
+            ) : (
+              <ProfileFallBack layout="fixed" />
+            )}
+          </Box>
+
+          <PersonInfoCard data={data} />
+        </Box>
+      </Backdrop>
+    </>
+  );
+};
+
+MediaDetailsPage.defaultProps = {
   backBtnPath: "/",
 };
 
-DetailsPage.propTypes = {
+MediaDetailsPage.propTypes = {
   data: PropTypes.object.isRequired,
   backBtnPath: PropTypes.string.isRequired,
 };
 
-export default DetailsPage;
+PersonDetailsPage.defaultProps = {
+  backBtnPath: "/",
+};
+
+PersonDetailsPage.propTypes = {
+  data: PropTypes.object.isRequired,
+  backBtnPath: PropTypes.string.isRequired,
+};
