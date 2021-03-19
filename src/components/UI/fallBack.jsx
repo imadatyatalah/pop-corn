712cd962273384651a54b557f8a39a659900b1b8
imad <imadatyatalah@gmail.com>
2021-03-19 11:02:24 +0100

    Added fallback image for profiles (people)

--- src/components/UI/fallBack.jsx
+++ src/components/UI/fallBack.jsx
@@ -1,12 +1,25 @@
-import { StyledPosterFallBack } from "./styles/fallBack.styles";
+import { StyledMediaFallBack } from "./styles/fallBack.styles";
 
 export const PosterFallBack = (props) => (
   <>
-    <StyledPosterFallBack
+    <StyledMediaFallBack
       src="/image.svg"
       width="342"
       height="513"
       alt="image-icon ionicons ionicons-icon"
+      quality="50"
+      {...props}
+    />
+  </>
+);
+
+export const ProfileFallBack = (props) => (
+  <>
+    <StyledMediaFallBack
+      src="/person-sharp.svg"
+      width="342"
+      height="513"
+      alt="person-sharp-icon ionicons ionicons-icon"
       quality="50"
       {...props}
     />
