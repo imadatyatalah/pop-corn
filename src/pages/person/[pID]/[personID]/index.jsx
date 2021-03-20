833781dde72343930b3171cd9abd82ccf2466406
imad <imadatyatalah@gmail.com>
2021-03-20 13:49:49 +0100

    Fetch combinde credits and add backdropPath prop

--- src/pages/person/[pID]/[personID]/index.jsx
+++ src/pages/person/[pID]/[personID]/index.jsx
@@ -3,7 +3,7 @@ import { dehydrate } from "react-query/hydration";
 import { useRouter } from "next/router";
 
 import { PersonDetailsPage as HeroSection } from "../../../../components/detailsPage/heroSection";
-import { getPersonDetails } from "../../../../lib/people";
+import { getCombinedCredits, getPersonDetails } from "../../../../lib/people";
 
 const Person = () => {
   const router = useRouter();
@@ -12,6 +12,10 @@ const Person = () => {
   const { data } = useQuery(["personID", query.personID], () =>
     getPersonDetails(query.personID)
   );
+  const { data: combinedCredits } = useQuery(
+    ["personID", "combinedCredits", query.personID],
+    () => getCombinedCredits(query.personID)
+  );
 
   if (isFallback) {
     return <div>Please wait a few seconds, It's loading...</div>;
@@ -19,7 +23,14 @@ const Person = () => {
 
   return (
     <>
-      <HeroSection data={data} backBtnPath={`/person/${query.pID}`} />
+      <HeroSection
+        data={data}
+        backBtnPath={`/person/${query.pID}`}
+        backdropPath={
+          combinedCredits.cast.sort((a, b) => b.popularity - a.popularity)[0]
+            .backdrop_path
+        }
+      />
     </>
   );
 };
@@ -33,6 +44,10 @@ export const getStaticProps = async ({ params }) => {
 
   await queryClient.prefetchQuery(["personID", params.personID], () =>
     getPersonDetails(params.personID)
+  );
+  await queryClient.prefetchQuery(
+    ["personID", "combinedCredits", params.personID],
+    () => getCombinedCredits(params.personID)
   );
 
   return {
