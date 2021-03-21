76d9f925a0e6f6c8b7125b707b1ced0008d829c4
imad <imadatyatalah@gmail.com>
2021-03-21 11:28:03 +0100

    Added NextSeo to Tv Shows pages

--- src/pages/tv/[pID]/index.jsx
+++ src/pages/tv/[pID]/index.jsx
@@ -1,9 +1,23 @@
 import { QueryClient, useQuery } from "react-query";
 import { dehydrate } from "react-query/hydration";
 import { useRouter } from "next/router";
+import { NextSeo } from "next-seo";
 
 import { getTvShows } from "../../../lib/tvShows";
 import { MediaCards as TvShowsCards } from "../../../components/UI/mediaCards";
+import config from "../../../../config";
+
+const getPageTitle = (pID) => {
+  if (pID === "popular") {
+    return "Popular";
+  } else if (pID === "airing_today") {
+    return "Airing Today";
+  } else if (pID === "on_the_air") {
+    return "On The Air";
+  } else if (pID === "top_rated") {
+    return "Top Rated";
+  }
+};
 
 const PID = () => {
   const router = useRouter();
@@ -13,8 +27,20 @@ const PID = () => {
     getTvShows(query.pID, 1)
   );
 
+  const title = `${getPageTitle(query.pID)} Tv Shows - ${config.title}`;
+  const description =
+    "Get the most Popular Tv Shows, Airing Today Tv Shows, On The Air Tv Shows and also Top Rated Tv Shows!";
+  const url = `${config.canonical}tv/${query.pID}`;
+
   return (
     <>
+      <NextSeo
+        title={title}
+        description={description}
+        canonical={url}
+        openGraph={{ title, description, url }}
+      />
+
       <TvShowsCards data={data} mediaType="tv" pID={query.pID} />
     </>
   );
