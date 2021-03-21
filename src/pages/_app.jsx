b017d7b6443f4d6b302585faeec816ac45282dfa
imad <imadatyatalah@gmail.com>
2021-03-21 18:45:30 +0100

    Remove unnecessary fonts weights

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -10,9 +10,7 @@ import SEO from "../../next-seo.config";
 
 import "@fontsource/poppins/latin-300.css";
 import "@fontsource/poppins/latin-400.css";
-import "@fontsource/poppins/latin-500.css";
 import "@fontsource/poppins/latin-600.css";
-import "@fontsource/poppins/latin-700.css";
 
 const MyApp = ({ Component, pageProps }) => {
   const queryClientRef = useRef();
