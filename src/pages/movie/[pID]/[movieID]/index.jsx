5a0c1e354ce31bf5e35b02a7e7611d98b57d1aa2
imad <imadatyatalah@gmail.com>
2021-03-21 18:28:24 +0100

    Added NextSeo to movie details pages

--- src/pages/movie/[pID]/[movieID]/index.jsx
+++ src/pages/movie/[pID]/[movieID]/index.jsx
@@ -1,9 +1,15 @@
 import { QueryClient, useQuery } from "react-query";
 import { dehydrate } from "react-query/hydration";
 import { useRouter } from "next/router";
+import { NextSeo } from "next-seo";
 
 import { MediaDetailsPage as HeroSection } from "../../../../components/detailsPage/heroSection";
 import { getMovieDetails } from "../../../../lib/movies";
+import config, {
+  IMAGE_BASE_URL,
+  LG_POSTER_SIZE,
+  MD_BACKDROP_SIZE,
+} from "../../../../../config";
 
 const Movie = () => {
   const router = useRouter();
@@ -17,8 +23,37 @@ const Movie = () => {
     return <div>Please wait a few seconds, It's loading...</div>;
   }
 
+  const title = `${data.title} - ${config.title}`;
+  const description = data.overview;
+  const url = `${config.canonical}movie/${query.pID}/${query.movieID}`;
+
   return (
     <>
+      <NextSeo
+        title={title}
+        description={description}
+        canonical={url}
+        openGraph={{
+          title,
+          description,
+          url,
+          images: [
+            {
+              url: `${IMAGE_BASE_URL}${LG_POSTER_SIZE}${data.poster_path}`,
+              width: "342",
+              height: "513",
+              alt: `${data.title} poster`,
+            },
+            {
+              url: `${IMAGE_BASE_URL}${MD_BACKDROP_SIZE}${data.backdrop_path}`,
+              width: "1280",
+              height: "720",
+              alt: `${data.title} backdrop`,
+            },
+          ],
+        }}
+      />
+
       <HeroSection data={data} backBtnPath={`/movie/${query.pID}`} />
     </>
   );
