4e1263c52ab5ad1479986febede1680f32939006
imad <imadatyatalah@gmail.com>
2021-03-21 18:41:40 +0100

    Added NextSeo to tv shows details pages

--- src/pages/tv/[pID]/[TvShowID]/index.jsx
+++ src/pages/tv/[pID]/[TvShowID]/index.jsx
@@ -1,9 +1,15 @@
 import { QueryClient, useQuery } from "react-query";
 import { dehydrate } from "react-query/hydration";
 import { useRouter } from "next/router";
+import { NextSeo } from "next-seo";
 
 import { MediaDetailsPage as HeroSection } from "../../../../components/detailsPage/heroSection";
 import { getTvShowDetails } from "../../../../lib/tvShows";
+import config, {
+  IMAGE_BASE_URL,
+  LG_POSTER_SIZE,
+  MD_BACKDROP_SIZE,
+} from "../../../../../config";
 
 const TvShow = () => {
   const router = useRouter();
@@ -17,8 +23,37 @@ const TvShow = () => {
     return <div>Please wait a few seconds, It's loading...</div>;
   }
 
+  const title = `${data.name} - ${config.title}`;
+  const description = data.overview;
+  const url = `${config.canonical}tv/${query.pID}/${query.TvShowID}`;
+
   return (
     <>
+      <NextSeo
+        title={title}
+        description={description}
+        canonical={url}
+        openGraph={{
+          title,
+          description,
+          url,
+          images: [
+            {
+              url: `${IMAGE_BASE_URL}${LG_POSTER_SIZE}${data.poster_path}`,
+              width: "342",
+              height: "513",
+              alt: `${data.name} poster`,
+            },
+            {
+              url: `${IMAGE_BASE_URL}${MD_BACKDROP_SIZE}${data.backdrop_path}`,
+              width: "1280",
+              height: "720",
+              alt: `${data.name} backdrop`,
+            },
+          ],
+        }}
+      />
+
       <HeroSection data={data} backBtnPath={`/tv/${query.pID}`} />
     </>
   );
